429e3eb931fc7c02ad3d668fcdec633a029a3235
Toni Sente <toni.sente@gmail.com>
2017-12-09 19:00:10 +0100

    Testing tcp receiver and sender class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        TCPReceiver receiver = new TCPReceiver(8080, "localhost");
+        TCPSender sender = new TCPSender(8080, "localhost");
+
+        new Thread(receiver).start();
+
+        try {
+            System.out.println("zzz .... ");
+            Thread.sleep(2000);
+        } catch (InterruptedException ignorable) {}
+
+        new Thread(sender).start();
     }
 }
