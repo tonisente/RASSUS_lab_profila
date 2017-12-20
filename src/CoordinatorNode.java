5e93308b6170858b913cc4ecd17b9ecd0471d7ad
Janko <janko.salkovic@fer.hr>
2017-12-20 10:42:16 +0100

    Radim na cvoru za koordiniranje(onaj koji zna stanje sustava).

---
+++ src/CoordinatorNode.java
@@ -0,0 +1,19 @@
+/**
+ * Server node that takes record of all the available nodes in the distributed
+ * system. Takes record if node enters the system, or if node leaves the system.
+ * Every node entering the system has to reach this node so it can get information about system
+ * state.
+ * 
+ * @author Janko
+ *
+ */
+public class CoordinatorNode {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
