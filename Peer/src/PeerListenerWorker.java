aecb02a99a00c7aa5b0d45274565abff2129c944
Toni Sente <toni.sente@gmail.com>
2018-01-09 18:51:21 +0100

    Small fixes is documentation and code

--- Peer/src/PeerListenerWorker.java
+++ Peer/src/PeerListenerWorker.java
@@ -5,7 +5,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class PeerListenerWorker implements Runnable {
 
-    private static final int SLEEP_TIME = 3000; // ms
+    private static final int SLEEP_TIME = 1500; // ms
 
     private AtomicBoolean runningFlag;
     private Socket clientSocket;
@@ -20,12 +20,6 @@ public class PeerListenerWorker implements Runnable {
         try {// create a new BufferedReader from an existing InputStream
              BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            System.out.print("INFO: ");
-            System.out.print(clientSocket.getLocalPort());
-            System.out.print(" :: ");
-            System.out.println(clientSocket.getInetAddress());
-            System.out.println();
-
              // create a PrintWriter from an existing OutputStream
              PrintWriter outToClient = new PrintWriter(new OutputStreamWriter(
                      clientSocket.getOutputStream()), true);
@@ -34,20 +28,59 @@ public class PeerListenerWorker implements Runnable {
 
             // read a few lines of text
             while ((receivedString = inFromClient.readLine()) != null/*READ*/) {
-                System.out.println("Server received: " + receivedString);
-
+                // sleep a little bit for simulation
                 try {
                     Thread.sleep(SLEEP_TIME);
                 } catch (InterruptedException ignore) {}
 
-                String stringToSend = "Listener odgovara!";
+                String stringToSend = MessageDecider(receivedString);
 
                 // send a String then terminate the line and flush
-                outToClient.println(stringToSend);//WRITE
+                if (!stringToSend.isEmpty()) {
+                    outToClient.println(stringToSend);//WRITE
+                }
+
+                break; // while loop
             }
         } catch (SocketTimeoutException ignore_and_continue) {
         } catch (IOException ex) {
             System.err.println("Exception caught when trying to read or send data: " + ex);
         }
     }
+
+    private String MessageDecider(String receivedString) {
+        String[] components = receivedString.split(";");
+        String answer = "";
+
+
+        switch (components[0]) {
+            case "4":
+                answer = AddNeighbour(components[1], components[2]);
+                break;
+            case "5":
+                answer = TreeCreation(components[1], components[2]);
+                break;
+            case "6":
+                SendMessage(components[1]);
+                break;
+            default:
+                System.out.println("Recieved message is invalid!");
+                break;
+        }
+
+        return answer;
+    }
+
+    private void SendMessage(String component) {
+        // send 
+    }
+
+    }    private String AddNeighbour(String component, String component1) {
+        return null;
+    }
+
+    private String AddNeighbour(String component, String component1) {
+        return null;
+
+
 }
